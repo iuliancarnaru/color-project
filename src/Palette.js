d8a752a973e1aa79a243dd5f1abb6ffa31eb35a3
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-07-11 22:16:34 +0100

    working with one plaette

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Palette extends Component {
+  render() {
+    return (
+      <div className="palette">
+        {/* Navbar */}
+        <div className="palette-colors">{/* Colors */}</div>
+        {/* Footer */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
