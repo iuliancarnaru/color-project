e67c7771d7a13c8cd5d2719f2bb703f488866729
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-07-13 13:07:34 +0100

    adding materialUI

--- src/Palette.js
+++ src/Palette.js
@@ -7,25 +7,34 @@ class Palette extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { level: 500 };
+    this.state = { level: 500, format: "hex" };
     this.changeLevel = this.changeLevel.bind(this);
+    this.changeFromat = this.changeFromat.bind(this);
   }
 
   changeLevel(level) {
     this.setState({ level });
   }
 
+  changeFromat(value) {
+    this.setState({ format: value });
+  }
+
   render() {
     const { colors } = this.props.palette;
-    const { level } = this.state;
+    const { level, format } = this.state;
 
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color.hex} name={color.name} />
+      <ColorBox background={color[format]} name={color.name} />
     ));
 
     return (
       <div className="palette">
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFromat}
+        />
         {/* Navbar */}
         <div className="palette-colors">{colorBoxes}</div>
         {/* Footer */}
