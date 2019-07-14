61959c503e5a5234c2909d66d6607c4e7bad23bc
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-07-14 14:03:43 +0100

    adding links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import generatePalette from "./colorHelpers";
+import PaletteList from "./PaletteList";
 
 function App() {
   const findPalette = id => {
@@ -13,7 +14,11 @@ function App() {
 
   return (
     <Switch>
-      <Route exact path="/" render={() => <h1>Palette list goes here</h1>} />
+      <Route
+        exact
+        path="/"
+        render={() => <PaletteList palettes={seedColors} />}
+      />
       <Route
         exact
         path="/palette/:id"
